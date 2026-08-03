7cc39cf42ac5a2710e18e9651f64cd60c736e086
Luca de Sampaio Callegari <72371512+Callegari152@users.noreply.github.com>
2026-08-03 19:21:15 -0300

    Delete Reorganização do diretorio

--- Projeto JustDoIt/backend/src/app.js
+++
@@ -1,9 +0,0 @@
-import authRoutes from "./routes/authRoutes.js";
-import express from "express";
-
-const app = express();
-
-app.use(express.json());
-app.use("/api/auth", authRoutes);
-
-export default app;
